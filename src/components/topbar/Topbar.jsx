b485226abf744be57e037462c5fd879414388742
Matthew Lock <matthew_lock20@hotmail.com>
2022-04-07 19:14:04 +0100

    Left topbar formatting complete

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,4 +1,5 @@
 import "./topbar.scss"
+import { Mail } from "@material-ui/icons"
 
 export default function Topbar() {
   return (
@@ -6,10 +7,12 @@ export default function Topbar() {
       <div className="wrapper">
         <div className="left">
           <a href='#intro' className="handle">MattyBwoy</a>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>matthew_lock20@hotmail.com</span>
+          </div>
         </div>
-        <div className="itemContainer">
-          
-        </div>
+        
         <div className="right">
           This is Right
         </div>
