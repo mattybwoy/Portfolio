fbf37b180da6dda6f08b9674b5876e6f5a1946a7
Matthew Lock <matthew_lock20@hotmail.com>
2022-04-12 17:56:55 +0100

    Added dummy projects and laid out CSS

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,38 @@ import "./portfolio.scss"
 export default function Portfolio() {
   return (
     <div className='portfolio' id='portfolio'>
-
+      <h1>Portfolio</h1>
+      <ul>
+        <li className="active">Website</li>
+        <li>Web App</li>
+        <li>Mobile</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="assets/Lofi_girl_logo.jpeg" alt=""/>
+        <h3>Lofi Radio</h3>
+        </div>
+        <div className="item">
+          <img src="assets/Lofi_girl_logo.jpeg" alt="" />
+          <h3>Lofi Radio</h3>
+        </div>
+        <div className="item">
+          <img src="assets/Lofi_girl_logo.jpeg" alt="" />
+          <h3>Lofi Radio</h3>
+        </div>
+        <div className="item">
+          <img src="assets/Lofi_girl_logo.jpeg" alt="" />
+          <h3>Lofi Radio</h3>
+        </div>
+        <div className="item">
+          <img src="assets/Lofi_girl_logo.jpeg" alt="" />
+          <h3>Lofi Radio</h3>
+        </div>
+        <div className="item">
+          <img src="assets/Lofi_girl_logo.jpeg" alt="" />
+          <h3>Lofi Radio</h3>
+        </div>
+      </div>
     </div>
   )
 }
