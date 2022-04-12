ff732157f0b8adbef870bb71092c3aed851789aa
Matthew Lock <matthew_lock20@hotmail.com>
2022-04-12 18:23:13 +0100

    Categories can now be selected in portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,13 +1,31 @@
-import "./portfolio.scss"
+import PortfolioList from "../portfolioList/PortfolioList";
+import "./portfolio.scss";
+import { useState } from "react";
 
 export default function Portfolio() {
+  const [selected, setSelected] = useState("website")
+  const list = [
+    {
+      id: "website",
+      title: "Website"
+    },
+    {
+      id: "web",
+      title: "Web App"
+    },
+    {
+      id: "mobile",
+      title: "Mobile App"
+    }
+  ]
+
   return (
     <div className='portfolio' id='portfolio'>
       <h1>Portfolio</h1>
       <ul>
-        <li className="active">Website</li>
-        <li>Web App</li>
-        <li>Mobile</li>
+      {list.map((item) => (
+        <PortfolioList title = {item.title} active={selected === item.id} setSelected={setSelected} id={item.id}/>
+      ))}
       </ul>
       <div className="container">
         <div className="item">
