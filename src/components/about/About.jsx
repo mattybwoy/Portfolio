f4f12941148abb97711dd6e403348effc028f562
Matthew Lock <matthew_lock20@hotmail.com>
2022-04-27 20:07:13 +0100

    Fleshed out about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,20 @@ import './about.scss'
 export default function About() {
   return (
     <div className='about' id='about'>
-      
+      <h1>About</h1>
+      <div className='top'>
+        <div className='imageContainer'>
+          <img src='/assets/iceBear2.png' alt='' />
+        </div>
+      </div>
+      <div className='middle'>
+        My name is Matthew and I have resurfaced as a software developer. 
+      </div>
+      <div className='bottom'>
+        Likes
+        
+      </div>
+      <img src='/assets/coffee.png' alt='' />
     </div>
   )
 }
