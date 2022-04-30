26aae37f7888ffae4b7cf4a4bfc6151301f60dcf
Matthew Lock <matthew_lock20@hotmail.com>
2022-04-30 09:08:59 +0100

    Added icons to about page and with brief description

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -10,13 +10,25 @@ export default function About() {
         </div>
       </div>
       <div className='middle'>
-        My name is Matthew and I have resurfaced as a software developer. 
+        My name is Matthew, resurfacing as a software developer.
+        A former Makers alumni with a passion for learning, coding, creating new projects and always challenging myself.
+        A full stack developer with native iOS skills, I'm currently working on building more apps within iOS.
+
+        I can tell you I don't have money, but what I do have are a very particular set of skills. Skills I have acquired over a very long career. Skills that make me a nightmare for people like you to handle.
+
       </div>
       <div className='bottom'>
         Likes
-        
+        <img src ='assets/heart.png' alt=''/>
       </div>
-      <img src='/assets/coffee.png' alt='' />
+      <div className='pictures'>
+        <img src='/assets/bubble-tea.png' alt=''/>
+        <img src='/assets/book.png' alt='' />
+        <img src='/assets/binary-code.png' alt='' />
+        <img src='/assets/badminton.png' alt='' />
+        <img src='/assets/suitcase.png' alt='' />
+      </div>
     </div>
+
   )
 }
