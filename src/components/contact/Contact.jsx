92deb4e480cd3dbf069e419b6f4b6a3bf97e1250
Matthew Lock <matthew_lock20@hotmail.com>
2022-05-01 17:32:11 +0100

    Alternating messages dependant upon message validation

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,6 +5,7 @@ import emailjs from 'emailjs-com';
 export default function Contact() {
 
   const [message, setMessage] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(false)
 
   const form = useRef();
 
@@ -12,9 +13,11 @@ export default function Contact() {
     e.preventDefault();
 
     if (!e.target.name.value || !e.target.email.value || !e.target.message.value) {
-      console.log('error')
+      setMessage(false)
+      setErrorMessage(true)
     } else {
       emailjs.sendForm('service_2bw6f7j', 'template_ruettum', form.current, 'M9xCb82F0UrK3EDGl')
+      setErrorMessage(false)
       setMessage(true)
     }
     e.target.reset()
@@ -26,13 +29,14 @@ export default function Contact() {
         <img src="./assets/wbb.png" alt=''/>
       </div>
       <div className ="right">
-        <h2>Contact</h2>
+        <h2>Contact Me</h2>
         <form ref = {form} onSubmit={sendEmail}>
           <input type= "text" name = "name" placeholder="Name"/>
           <input type="text" name = "email" placeholder="Email"/>
           <textarea name = "message" placeholder = "Message"></textarea>
           <button type="submit">Send</button>
-          {message && <span>Thanks, I'll reply soon</span>}
+          {message && <span className="msg">Thanks, I'll reply soon</span>}
+          {errorMessage && <span className="error">Please fill in all fields</span>}
         </form>
       </div>
     </div>
